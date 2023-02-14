8d591a6a81a3f3deffe678884085484aa3cfa6f1
Lenah Abdulrahman Alhelal <108255709+lenahabdulrahman@users.noreply.github.com>
2023-02-14 16:05:36 +0300

    Add files via upload

---
+++ Find Food.pl
@@ -0,0 +1,360 @@
+                          
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% KB %%%%%%%%%%%%%%%%%%%%%%%%5%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            HEALTH CONDITION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%---------------------------- Diabetic --------------------------------
+% Diabetic breakfast
+meal_for("oatmeal", 1).
+meal_for("multigrain avocado toast", 1).
+meal_for("greek yougurt with berries", 1).
+
+% Diabetic lunch
+meal_for("homemade Veggie Stir-Fry With Brown Rice", 1).
+meal_for("whole-Wheat Wrap With Lean Protein and Avocado", 1).
+meal_for("tuna Salad on Whole-Grain Toast", 1).
+
+% Diabetic dinner
+meal_for("lemon garlic salmon", 1).
+meal_for("quinoa Salad", 1).
+meal_for("vegetarian Lentil Tacos", 1).
+
+%------------------------ Gluten sensitive --------------------------------
+
+% Gluten breakfast
+meal_for("coconut Flour Pancakes",2).
+meal_for("cloud Eggs",2).
+meal_for("breakfast Tomatoes",2).
+
+
+% Gluten lunch
+meal_for("shrimp Avocado Salad",2).
+meal_for("grilled Mango & Avocado Salad",2).
+meal_for("grilled Broccoli",2).
+
+
+% Gluten dinner
+meal_for("vegan Chili",2).
+meal_for("cheesesteak Stuffed Peppers",2).
+meal_for("garlicky Lemon Mahi Mahi",2).
+
+%---------------------------- Healthy --------------------------------
+
+% Healthy breakfast
+meal_for("avocado Toast and Egg",3).
+meal_for("warm Berry Bowl",3).
+meal_for("spinach and Cheddar Microwave Quiche",3).
+
+% Healthy lunch
+meal_for("roasted Cauliflower Tacos",3).
+meal_for("shaved Carrot and Radish Salad",3).
+meal_for("tomato Panzanella",3).
+
+
+% Healthy dinner
+meal_for("lemon chicken",3).
+meal_for("chopped chilli chicken stir fry",3).
+meal_for("raw pad Thai",3).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                              MEAL TYPE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                               
+
+%---------------------------- Diabetic --------------------------------
+
+% Diabetic breakfast
+meal_type("oatmeal", 1).
+meal_type("multigrain avocado toast", 1).
+meal_type("greek yougurt with berries", 1).
+
+% Diabetic lunch
+meal_type("homemade Veggie Stir-Fry With Brown Rice", 2).
+meal_type("whole-Wheat Wrap With Lean Protein and Avocado", 2).
+meal_type("tuna Salad on Whole-Grain Toast", 2).
+
+% Diabetic dinner
+meal_type("lemon garlic salmon", 3).
+meal_type("quinoa Salad", 3).
+meal_type("vegetarian Lentil Tacos", 3).
+
+%------------------------ Glutin sensitive --------------------------------
+
+% Glutin sensitive breakfast
+meal_type("coconut Flour Pancakes",1).
+meal_type("cloud Eggs",1).
+meal_type("breakfast Tomatoes",1).
+
+
+% Glutin sensitive lunch
+meal_type("shrimp Avocado Salad",2).
+meal_type("grilled Mango & Avocado Salad",2).
+meal_type("grilled Broccoli",2).
+
+
+% Glutin sensitive dinner
+meal_type("vegan Chili",3).
+meal_type("cheesesteak Stuffed Peppers",3).
+meal_type("garlicky Lemon Mahi Mahi",3).
+
+%---------------------------- Healthy --------------------------------
+
+% Healthy breakfast
+meal_type("avocado Toast and Egg",1).
+meal_type("warm Berry Bowl",1).
+meal_type("spinach and Cheddar Microwave Quiche",1).
+
+% Healthy lunch
+meal_type("roasted Cauliflower Tacos",2).
+meal_type("shaved Carrot and Radish Salad",2).
+meal_type("tomato Panzanella",2).
+
+% Healthy dinner
+meal_type("lemon chicken",3).
+meal_type("chopped chilli chicken stir fry",3).
+meal_type("raw pad Thai",3).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                 RECIPES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% recipes
+recipe("oatmeal"," 3/4 cup oats,2 eggs,1/2 cup milk,1 Tablespoon ground flaxseed,1 teaspoon cinnamon,1 banana mashed, Mix all ingredients together in a pot on the stove; turn to medium-high heat. This will take about 5 minutes. ",1,1,1).
+recipe("multigrain avocado toast"," mashed avocado, drizzled with extra-virgin olive oil. chopped parsley is sprinkled on top. Add some red pepper flakes if you like a spicy kick. put it over multigrain bread",1,1,2).
+recipe("greek yougurt with berries","place ¾ cup of yogurt in a bowl. slice your berries, place them on top of the yogurt. Drizzle honey over top.",1,1,3).
+recipe("homemade Veggie Stir-Fry With Brown Rice","start with ⅔ cup of cooked brown rice. Top it with plenty of cooked vegetables like green peppers, onions, broccoli, bok choy, celery, and carrots; then add 3 to 5 oz of a lean protein like chicken, tofu, or beans. Steer clear of breaded or fried meats, which pack hidden carbs, Swift says. Cover your creation with 1 tablespoon of low-sodium soy sauce.",1,2,1).
+recipe("whole-Wheat Wrap With Lean Protein and Avocado","start with a whole-wheat tortilla or pita (look for one with no more than 30 g of carbs). Spread it with 1 tablespoon of hummus or pesto, then add 3 oz of a lean protein of your choice. Feel free with veggies like lettuce, tomatoes, cucumbers, bell peppers, and shredded carrots. Top with a few slices of avocado.",1,2,2).
+recipe("tuna Salad on Whole-Grain Toast","stir together one flavored tuna packet with a mini avocado, 1 tablespoon of olive oil-based mayo, and a ¼ cup of chopped veggies (onion, celery, or radish). Spoon the entire mixture on a slice of whole-grain toast",1,2,3).
+recipe("lemon garlic salmon"," whisk lemon juice, garlic, oil, salt, and pepper into a bowl. Place the salmon fillets over the lemon marinade. Let marinate for at least 30 minutes. Bake the salmon for about 12-15 minutes ",1,3,1).
+recipe("quinoa Salad","mix together the olive oil, garlic, lemon juice, vinegar, maple syrup or honey, salt, and pepper. In a large bowl, combine quinoa, spinach, cucumber, tomatoes, avocado, and green onions. Drizzle salad with dressing and gently stir.",1,3,2).
+recipe("vegetarian Lentil Tacos","lentils, tomatoes, onions, garlic, chili powders, and paprika can be added to a slow cooker. Cover and cook on high 3 - 4 hours on high. Stir in cilantro and lime juice; serve warm with tortillas or toppings.",1,3,3).
+recipe("coconut Flour Pancakes","mix together Greek yogurt, egg yolks, maple syrup, and melted butter in a bowl. Add coconut flour, baking soda, and salt; gently fold until just combined. Grease a large nonstick pan with cooking spray and place over medium-low heat.",2,1,1).
+recipe("cloud Eggs","beat egg whites and yolks in a bowl; fold in Parmesan and ham; season with salt and pepper. Bake until lightly golden, about 3 minutes. Garnish with chives before serving.",2,1,2).
+recipe("breakfast Tomatoes","preheat oven to 400° and line a baking sheet with parchment paper. Slice tops off tomatoes and hollow with a metal spoon. Drizzle with olive oil and season with salt and pepper. Bake 10 minutes, until softened slightly. Crack eggs into center and place back in oven to bake 12 more minutes.",2,1,3).
+recipe("shrimp Avocado Salad","green onions, shrimp, serrano pepper, and lime juice. Mix ingredients together in a large bowl; mix to combine flavors. Refrigerate until flavors blend, about 1 hour.",2,2,1).
+recipe("grilled Mango & Avocado Salad","garnish mangoes with lime juice, olive oil, cucumbers and avocados. Cook mangoes over medium heat or broil for 6-8 minutes or until lightly browned, turning once.",2,2,2).
+recipe("grilled Broccoli","prepare grill for indirect heat using a drip pan. Place broccoli over drip pan on an oiled grill rack. Grill, covered, over indirect medium heat for 8-10 minutes on each side or until crisp-tender. If desired, garnish with grilled lemon slices and red pepper flakes.",2,2,3).
+recipe("vegan Chili","add beer and bring to a boil, then reduce to a simmer until it has reduced by half. Cook until sweet potatoes are cooked through, 30 to 35 minutes; serve with toppings of your choice.",2,3,1).
+recipe("cheesesteak Stuffed Peppers","method: Preheat oven to 325º. Add onions and mushrooms to a large baking dish and bake until tender, 30 minutes. Add provolone to bottom of baked peppers and top with steak mixture. Broil until golden, 3 minutes. Garnish with parsley before serving.",2,3,2).
+recipe("garlicky Lemon Mahi Mahi","in a large skillet over medium heat, melt butter and olive oil. Add mahi-mahi and season with salt and pepper. Cook until golden, 4 to 5 minutes per side, then transfer to a plate. Add asparagus and cook until tender, 2 to 4 minutes.",2,3,3).
+recipe("avocado Toast and Egg","prepare toast and fried eggs.Peel,mash avocado with lime juice, salt and pepper.Spread avocado toast topp it with egg and prefered seasoning.",3,1,1).
+recipe("warm Berry Bowl","on baking sheet, pour fruit with cinnamon and pinch of sea salt, stir to evenly spread.Bake for 20 minutes. Take out of the oven and sprinkle optional garnishes and dark chocolate on top.Serve in a small bowl, pour milk on top",3,1,2).
+recipe("spinach and Cheddar Microwave Quiche","using fresh spinach, place it in mug with 2 tablespoons of water and microwave on high for one minute. Add milk, cheese, bacon, salt and pepper and mix until combined. Remove from microwave.",3,1,3).
+recipe("roasted Cauliflower Tacos","roast cauliflower on a rimmed baking sheet until it is golden brown and tender, 18 to 22 minutes. Spoon cauliflower into tortillas and top with slaw; add lime wedges and cilantro for garnish.",3,2,1).
+recipe("shaved Carrot and Radish Salad","in bowl, whisk together orange juice, lemon juice, honey, and 1/2 tsp each salt and pepper to dissolve honey, then whisk in oil. Arrange vegetables, oranges, and mint on large platter and drizzle with vinaigrette.",3,2,2).
+recipe("tomato Panzanella","roast bread, add olive oil, garlic, anchovies and basil, then toss with dressing; let stand at room temp for 30 minutes. Toss croutons with tomatoes, then fold in remaining basil leaves, then drizzle with extra oil and cracked pepper.",3,2,3).
+recipe("lemon chicken","method: Mix the cornflour with the soy sauce and lemon juice in a small bowl. Heat the oil in a frying pan or wok over a high heat and add the vegetables. Stir-fry for 2–3 minutes, or until the chicken is lightly browned.",3,3,1).
+recipe("chopped chilli chicken stir fry","heat oil in a wok or large frypan over medium-high heat. Add eggplants, season and cook, stirring, for 3-4 minutes until golden. Add onion, chilli paste and half the chilli and basil; cook for 2-3 minutes.",3,3,2).
+recipe("raw pad Thai","zucchini, carrot, capsicum, cabbage, snow peas, bean sprouts and edamame. Add the dressing and toss to combine. Divide among serving plates, top with the basil and serve with toasted seed mix.",3,3,3).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                          RECOMMENDATION CODE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%---------------------------- SUBMENUS --------------------------------
+%this is the start of the system
+start :-
+    write('Welcom to FOOD FIND ... is this your first time using FOOF FIND?'),nl,nl,
+    write('Enter the number of your option:'), nl,
+    write('========================='), nl,
+    write('1- Yes'),nl,
+    write('2- No'),nl,
+    write('========================='), nl,
+    validate_input_two_options(X),
+    X == 2 -> main_menu;begin.
+
+begin:-
+    nl,
+    write('------------------------------------------------------------------------'), nl,
+    write('FOOD FIND ia an expert system that assists you in '),nl,
+    write('finding the most suitable meal for your health condition  '),nl,
+    write('------------------------------------------------------------------------'), nl,nl,
+    write('Do you want to continue using FOOD FIND? '),nl,nl,
+    write('Enter the number of your option:'), nl,
+    write('========================='), nl,
+    write('1- Yes'),nl,
+    write('2- No'),nl,
+    write('========================='), nl,
+    validate_input_two_options(Y),
+    Y == 1 ->  main_menu;
+    exit_case.
+
+continue:-
+    write("Do you want to try another options?"),nl,
+    write('========================='), nl,
+    write('1- Yes'),nl,
+    write('2- No'),nl,
+    write('========================='), nl,
+    write('=========================================================================='), nl,
+    validate_input_two_options(X),
+    X == 1 , main_menu;exit_system.
+
+exit_case:-
+    nl,
+    write('========================='), nl,
+    write('Hope to see you soon!'),nl,
+    write('========================='), nl,abort.
+
+exit_system:-
+    nl,
+    write('=============================='), nl,
+    write('THANK YOU FOR USING FOOD FIND'),nl,
+    write('=============================='), nl,abort.
+
+validate_input_condition_type(N) :-
+    read(N),  % get user input
+    number(N),N>0, N<5, !  % exit goal, is it a number?
+    ; writeln('Please enter number between 1 and 4'), validate_input_condition_type(N).  % recursive call if not number
+
+validate_input_three_options(N) :-
+    read(N),  % get user input
+    number(N),N>0, N<4, !  % exit goal, is it a number?
+    ; writeln('Please enter number between 1 and 3'), validate_input_three_options(N).  % recursive call if not number
+
+validate_input_two_options(N) :-
+    read(N),  % get user input
+    number(N),N>0, N<3, !  % exit goal, is it a number?
+    ; writeln('Please enter 1 or 2'), validate_input_two_options(N).  % recursive call if not number
+
+validate_input_number(N) :-
+    read(N),  % get user input
+    number(N), !  % exit goal, is it a number?
+    ; writeln('Please enter valid input'), validate_input_number(N).  % recursive call if not number
+
+
+list1 :-
+    write('========================='), nl,nl,
+    write('Enter the number of your option:'), nl,
+    write('1 - Recommendation'), nl,
+    write('2 - Calories calculator'), nl,
+    write('3 - Exit'), nl,
+    write('========================='), nl, nl.
+
+ask_health_condition :-
+    write('========================='), nl,
+    write('Please select a health condition:'), nl,nl,
+    write('Enter the number of your option:'), nl,
+    write('1 - Dabetic'), nl,
+    write('2 - Glutin sensitive'), nl,
+    write('3 - Maintaining a healthy diet'), nl,
+    write('4 - Exit'), nl,
+    write('========================='), nl, nl.
+
+ask_meal_type :-
+    write('========================='), nl,
+    write('Please select type of meal:'), nl,nl,
+    write('Enter the number of your option:'), nl,
+    write('1 - Breakfast'), nl,
+    write('2 - Lunch'), nl,
+    write('3 - Dinner'), nl,
+    write('4 - Exit'), nl,
+    write('========================='), nl, nl.
+
+gender:-
+    write('========================='), nl,
+    write('Please select your gender:'), nl,nl,
+    write('Enter the number of your option:'), nl,
+    write('1 - Female'), nl,
+    write('2 - Male'), nl,
+    write('========================='), nl, nl.
+
+weight:-
+    write('Please enter your weight in kg:'), nl,nl.
+
+height:-
+    write('Please enter your height in cm:'), nl,nl.
+
+age:-
+    write('Please enter your age:'), nl,nl.
+
+%---------------------------- MENU -------------------------------
+main_menu:-
+    list1,
+    validate_input_three_options(Option),
+    (   Option == 1 -> menu;   
+    	Option == 2 -> calories;
+    	exit_case
+    ).
+%---------------------------- Recommnedation MENU -------------------------
+menu:-
+    ask_health_condition,
+    validate_input_condition_type(Health_Condition),
+    check_to_exit(Health_Condition),
+    ask_meal_type,
+	validate_input_condition_type(Meal_Typet),
+    check_to_exit(Meal_Typet),
+	recommend(Health_Condition,Meal_Typet),nl,
+    exit_system.
+
+check_to_exit(X):-
+    X == 4 , exit_case; write("").
+
+%---------------------------- calories function -------------------------
+fc(W,H,A):-
+    B is((10*W)+(6.25*H)-(5*A)-161),
+    write('The suggested calories to consume each day is: '),
+	print(B),nl,nl,
+    continue.
+    
+mc(W,H,A):-
+    B is((10*W)+(6.25*H)-(5*A)+5),
+    write('The suggested calories to consume each day is: '),
+	print(B),nl,nl,
+    continue.
+
+calories:-
+    gender,
+    validate_input_two_options(G),
+    weight,
+    validate_input_number(W),
+    height,
+    validate_input_number(H),
+    age,
+    validate_input_number(A),
+    ( G == 1 ->  fc(W,H,A); G == 2 ->  mc(W,H,A) ).
+
+
+%------------------------ RECOMENDATION PART --------------------------
+
+proper_meal(X,Y,Z):- meal_for(Z, X), meal_type(Z,Y).
+
+recommend(Health_Condition,Meal_Typet) :-
+    findall(Z,proper_meal(Health_Condition,Meal_Typet,Z),List),
+    write('=========================================================================='), nl,
+    write("We would recommend these meals for you:"),
+    print_list(List),nl,
+    write("Which recipe Would you like?(please enter a number): "),
+    validate_input_three_options(N),
+    recipe(Name,Y,Health_Condition,Meal_Typet,N),nl,
+    write("The recipe of <<"),write(Name),write(">> is: "),nl,
+    write('-------------------------------------------------------------------------------------------------------------------------------'), nl,
+    write(Y),nl,
+    write('-------------------------------------------------------------------------------------------------------------------------------'), nl,nl,continue.
+
+
+%------------------------ Printing numbered list -------------------------
+print_list(L) :-
+    nl,write('- '),
+    print_numbered_list(L).
+
+print_numbered_list([H1,H2|T]) :-
+    print(H1),
+    nl,write('- '),
+    print_numbered_list([H2|T]).
+print_numbered_list([Last_Element]) :-
+    print(Last_Element),
+    nl.
+print_numbered_list([]) :- nl.
+
+
+
+
+
+    
+
+
